d92434b8d4f889e87b0776d1e6c9cda43fcff4d6
Jemish patoliya <145182049+jemishpatoliya@users.noreply.github.com>
2026-01-06 00:55:57 +0530

    remove serve and fix railway

--- server.js
+++ server.js
@@ -3,38 +3,27 @@ const fs = require("fs");
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+const ROOT = __dirname;
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(
-    __dirname,
-    req.url === "/" ? "index.html" : req.url
-  );
+http.createServer((req, res) => {
+  let file = req.url === "/" ? "index.html" : req.url;
+  let filePath = path.join(ROOT, file);
+
+  if (!fs.existsSync(filePath)) {
+    filePath = path.join(ROOT, "index.html");
+  }
 
   const ext = path.extname(filePath);
-  const contentTypeMap = {
+  const types = {
     ".html": "text/html",
     ".css": "text/css",
     ".js": "application/javascript",
     ".png": "image/png",
-    ".jpg": "image/jpeg",
-    ".jpeg": "image/jpeg",
-    ".svg": "image/svg+xml",
-    ".json": "application/json"
+    ".jpg": "image/jpeg"
   };
 
-  const contentType = contentTypeMap[ext] || "text/plain";
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(404);
-      res.end("404 Not Found");
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content);
-    }
-  });
-});
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port:", PORT);
+  res.writeHead(200, { "Content-Type": types[ext] || "text/plain" });
+  fs.createReadStream(filePath).pipe(res);
+}).listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
 });
